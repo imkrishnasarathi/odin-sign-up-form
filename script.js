869516dabce8915f07fc685201b4f763613a3d09
imkrishnasarathi <krishnasarathishem@gmail.com>
2023-10-08 14:02:13 +0530

    upate

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const passwordField = document.getElementById("password");
-  const confirmPasswordField = document.getElementById("confirmPassword");
+const confirmPasswordField = document.getElementById("confirmPassword");
+const validationText = document.getElementById("validationText");
 
   function validatePassword() {
     const password = passwordField.value;
@@ -7,8 +8,15 @@ const passwordField = document.getElementById("password");
 
     if (password !== confirmPassword) {
       confirmPasswordField.setCustomValidity("Passwords must match");
+      confirmPasswordField.classList.add("error");
+      passwordField.classList.add("error");
+      validationText.textContent = "Passwords must match";
+
     } else {
       confirmPasswordField.setCustomValidity("");
+      confirmPasswordField.classList.remove("error");
+      passwordField.classList.remove("error");
+      validationText.textContent = "";
     }
   }
 
